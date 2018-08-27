f51fbefa1ff9e614dfd19f282853945eddabb4d6
Jan Wächter <jan@waechter-home.ch>
2018-08-27 18:17:58 +0200

    remove

--- SoftwareEngineering/Statistics/src/StatisticsTest.java
+++
@@ -1,9 +0,0 @@
-public class StatisticsTest {
-	public static void main(String[] args) {
-		Statistics statistics = new Statistics(3);
-		statistics.addNumber(10);
-		statistics.addNumber(20);
-		statistics.addNumber(30);
-		System.out.println("Average: " + statistics.getAverage());	
-	}	
-}
